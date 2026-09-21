ddd7485c6d513005d9ff81a2c7d2f86d63c754e8
valent-gif <valentinarojasmoratto@gmail.com>
2026-09-21 07:12:02 -0500

    cree el archivo calculadora y le agregue la funcion restar

--- matematicas/calculadora.py
+++ matematicas/calculadora.py
@@ -2,3 +2,10 @@ def suma_numeros (num1, num2):
     print(num1 + num2)
 
 suma_numeros(5, 10) 
+
+def resta_numeros ():
+    numero1 = input("Ingrese el primer número: ")
+    numero2 = input("Ingrese el segundo número: ")
+    print (numero1 - numero2)
+
+resta_numeros(10,3)
