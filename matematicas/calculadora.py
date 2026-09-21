bd33862597b7625a2fccaeede326baa9094d8c7c
valent-gif <valentinarojasmoratto@gmail.com>
2026-09-21 07:20:21 -0500

    feat: tenia un error en el codigo de la resta
    me flato agregar a la resta un int en la linea de codigo, por eso me generaba error

--- matematicas/calculadora.py
+++ matematicas/calculadora.py
@@ -4,15 +4,15 @@ def suma_numeros (num1, num2):
 suma_numeros(5, 10) 
 
 def resta_numeros ():
-    numero1 = input("Ingrese el primer número: ")
-    numero2 = input("Ingrese el segundo número: ")
+    numero1 = int(input("Ingrese el primer número: "))
+    numero2 = int(input("Ingrese el segundo número: "))
     print (numero1 - numero2)
 
 resta_numeros(10,3)
 
 def multiplicacion_numeros ():
-    numero1 = input("Ingrese el primer número: ")
-    numero2 = input("Ingrese el segundo número: ")
+    numero1 = int(input("Ingrese el primer número: "))
+    numero2 = int(input("Ingrese el segundo número: "))
     print (numero1 * numero2)
 
 multiplicacion_numeros(4, 5)
